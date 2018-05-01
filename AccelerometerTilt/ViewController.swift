1d549a07eca8de36fe13168e0928747e3f6a11e9
Yvonne Allen <yallen011@gmail.com>
2018-05-01 00:54:20 -0400

    added code to increment score when button is clicked and to move button around the screen when the screen is tilted.

--- AccelerometerTilt/ViewController.swift
+++ AccelerometerTilt/ViewController.swift
@@ -2,7 +2,6 @@
 //  ViewController.swift
 //  AccelerometerTilt
 //
-//  Created by Allen, Yvonne on 4/30/18.
 //  Copyright © 2018 Learning Mobile Apps. All rights reserved.
 //
 
@@ -13,16 +12,28 @@ class ViewController: UIViewController {
     
     let motionManager = CMMotionManager()
     var timer: Timer!
+    var counter:Int = 0
 
+    @IBOutlet weak var lblScore: UILabel!
+    @IBOutlet weak var btnTiltOutlet: UIButton!
+    @IBAction func btnTilt(_ sender: Any) {
+        counter += 1
+        lblScore.text = String(counter)
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         
+       
+        counter = 0
+        
+        motionManager.accelerometerUpdateInterval = 0.05
         motionManager.startAccelerometerUpdates()
         motionManager.startGyroUpdates()
         motionManager.startMagnetometerUpdates()
         motionManager.startDeviceMotionUpdates()
         
-        timer = Timer.scheduledTimer(timeInterval: 3.0, target: self, selector: #selector(ViewController.update), userInfo: nil, repeats: true)
+        timer = Timer.scheduledTimer(timeInterval: 0.05, target: self, selector: #selector(ViewController.update), userInfo: nil, repeats: true)
     }
 
     override func didReceiveMemoryWarning() {
@@ -32,6 +43,23 @@ class ViewController: UIViewController {
     
     @objc func update() {
         if let accelerometerData = motionManager.accelerometerData {
+            let acceleration = accelerometerData.acceleration
+            let moveFactor = 15
+            var rect: CGRect  = btnTiltOutlet.frame
+            let moveToX = rect.origin.x + CGFloat((acceleration.x * Double(moveFactor)))          //3
+            let moveToY = (rect.origin.y + rect.size.height) - CGFloat((acceleration.y * Double(moveFactor)))                                                                    //4
+            let maxX = self.view.frame.size.width - rect.size.width                    //5
+            let maxY = self.view.frame.size.height                                     //6
+            if(moveToX > 0 && moveToX < maxX){                                            //7
+                rect.origin.x += CGFloat((acceleration.x * Double(moveFactor)))
+            }
+            if(moveToY > rect.size.height && moveToY < maxY){                             //8
+                rect.origin.y -= CGFloat((acceleration.y * Double(moveFactor)))
+            }
+            
+            UIView.animate(withDuration: 0, delay: 0, options: [.curveEaseInOut], animations: {
+                self.btnTiltOutlet.frame = rect;
+            }, completion: nil)
             print(accelerometerData)
         }
         if let gyroData = motionManager.gyroData {
